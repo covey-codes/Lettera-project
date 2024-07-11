ec9f5cedd17382f6e92152c3406fbb7d85c080c1
kelvin Godwin Efosa <dustindark7@gmail.com>
2024-07-11 23:43:53 +0100

    third commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        Lettera makes letter-writting so 
+        <span className="bg-gradient-to-r from-orange-500 to-red-800 ">much easier</span>
+      </h1>
+    </div>
+  );
+  
+};
+
+export default HeroSection;
+
