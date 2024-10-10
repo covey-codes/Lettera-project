67483b2944a9292f89445f66474cc3b2d0dae22c
kelvin Godwin Efosa <dustindark7@gmail.com>
2024-10-10 15:59:44 +0100

    8th commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
 import Workflow from "./components/Workflow";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
         <HeroSection />
         <FeatureSection />
         <Workflow />
+        <Footer />
       </div>
     </>
   );
